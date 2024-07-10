e7d1cf38674fa963d47a82fe78c4984afbaef6eb
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-07-11 00:18:10 +0800

    creating gui with configurable options for sphere object

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -51,11 +52,34 @@ scene.add(sphere);
 
 sphere.position.set(-10, 10, 0);
 
+const gui = new dat.GUI();
+
+const options = {
+    sphereColor: "#ffea00",
+    wireframe: false,
+    speed: 0.01
+};
+
+gui.addColor(options, "sphereColor").onChange(function(e){
+    sphere.material.color.set(e);
+});
+
+gui.add(options, 'wireframe').onChange(function(e){
+    sphere.material.wireframe = e;
+});
+
+gui.add(options, 'speed', 0, 0.1);
+
+let step = 0;
+
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
+
+    step += options.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(step));
+
     renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate); // time is automatically set by the browser. time represents the amount of milliseconds elapsed since document load
-
