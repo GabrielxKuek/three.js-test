6240123994243326a325e1c452245d9f6508f811
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-07-10 23:46:16 +0800

    experimentation with different geometries and materials

--- dist/index.64a4978e.js
+++ dist/index.64a4978e.js
@@ -602,15 +602,30 @@ const boxMaterial = new _three.MeshBasicMaterial({
 });
 const box = new _three.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
-box.rotation.x = 5;
-box.rotation.y = 5;
+const planeGeometry = new _three.PlaneGeometry(30, 30);
+const planeMaterial = new _three.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side: _three.DoubleSide
+});
+const plane = new _three.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+const gridHelper = new _three.GridHelper(30);
+scene.add(gridHelper);
+const sphereGeometry = new _three.SphereGeometry(4, 50, 50);
+const sphereMaterial = new _three.MeshBasicMaterial({
+    color: 0x0000FF,
+    wireframe: false
+});
+const sphere = new _three.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+sphere.position.set(-10, 10, 0);
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
     renderer.render(scene, camera);
 }
-renderer.setAnimationLoop(animate) // time is automatically set by the browser. time represents the amount of milliseconds elapsed since document load
-;
+renderer.setAnimationLoop(animate); // time is automatically set by the browser. time represents the amount of milliseconds elapsed since document load
 
 },{"three":"ktPTu","three/examples/jsm/controls/OrbitControls.js":"7mqRv"}],"ktPTu":[function(require,module,exports) {
 /**
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -29,8 +29,27 @@ const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
-box.rotation.x = 5;
-box.rotation.y = 5;
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000FF,
+    wireframe: false
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+
+sphere.position.set(-10, 10, 0);
 
 function animate(time) {
     box.rotation.x = time / 1000;
@@ -38,5 +57,5 @@ function animate(time) {
     renderer.render(scene, camera);
 }
 
-renderer.setAnimationLoop(animate) // time is automatically set by the browser. time represents the amount of milliseconds elapsed since document load
+renderer.setAnimationLoop(animate); // time is automatically set by the browser. time represents the amount of milliseconds elapsed since document load
 
