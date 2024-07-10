f90036e4d7be6e117eb7672df424ab39f3044d0e
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-07-10 23:03:00 +0800

    created scene, added camera and orbit controls

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,42 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
-const RENDERER = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 
-RENDERER.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-document.body.appendChild(RENDERER.domElement);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(0, 2, 5);
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+box.rotation.x = 5;
+box.rotation.y = 5;
+
+function animate(time) {
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate) // time is automatically set by the browser. time represents the amount of milliseconds elapsed since document load
+
